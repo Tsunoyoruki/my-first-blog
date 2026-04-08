db6e9efcaaf337de431498c9f6305e03cbeab0c8
eSu <tsunoyoruki@gmail.com>
2026-04-09 04:37:57 +0800

    test debug false

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)w$b%h)j-(syumz++6)2oi&-ks2rgcayvx5o_cc^ozov+frx%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
